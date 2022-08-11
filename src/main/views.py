a660a15f661e930e152a8684fc5eb0a813f845df
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-11 23:09:52 +0800

    Add @login_required decorator

--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# @login_required(login_url='login')
 
-
+@login_required
 def main(request):
     return render(request, 'main/main.html')
