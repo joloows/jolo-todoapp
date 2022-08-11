9b1c593faf9a65ac46fffc7250d77a88a59b4f3a
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-11 23:08:00 +0800

    Refractor routing for all apps

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LoginView
+from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
