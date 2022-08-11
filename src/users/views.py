61bdd8f1d5c1cd51ec8f9b3bde565758c16a3e1a
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-11 23:19:56 +0800

    Add views
    Added login_view, logout_view, register_view which handles user authentications.

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,40 @@
-from re import template
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 
-class LoginView(TemplateView):
-    template_name = 'users/login.html'
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, 'Invalid login.')
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid:
+            form.save()
+            user = form.cleaned_data['username']
+            messages.success(
+                request, f'Successfully created account for {user}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+        context = {'form': form}
+        return render(request, 'users/register.html', context)
