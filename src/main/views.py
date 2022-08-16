5a60a97624f6d28826cea788c4e14da6406e3eea
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-16 17:19:55 +0800

    Add render context

--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from main.models import Todo, Task
 
 
 @login_required
 def main(request):
-    return render(request, 'main/main.html')
+    user = request.user
+    todo = Todo.objects.filter(user=user)
+    context = {
+        'todos': todo
+    }
+    return render(request, 'main/main.html', context)
