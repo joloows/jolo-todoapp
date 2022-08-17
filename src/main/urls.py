230cade6db3fbb42c96ba6f88858c9e2f1efa2d3
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-17 22:54:24 +0800

    Add Ajax for checkbox
    Created a view and template for Task objects. Ajax POST request parameters:
    '_id' - object.id
    'value' - custom value 'true' or 'false'

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('main/', views.main, name='main'),
-]
+    path('main/<int:todo_id>/', views.tasks_view, name='todo-view'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/main/views.py
+++ src/main/views.py
@@ -1,13 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from main.models import Todo, Task
+
+from .models import Todo, Task
 
 
 @login_required
 def main(request):
+    if request.method == 'POST':
+        todo_id = request.POST.get('_id')
+        todo = Todo.objects.get(id=todo_id)
+        if request.POST.get('value') == 'true':
+            todo.todo_is_finished = True
+        else:
+            todo.todo_is_finished = False
+        todo.save()
+
     user = request.user
     todo = Todo.objects.filter(user=user)
     context = {
-        'todos': todo
+        'todos': todo,
     }
     return render(request, 'main/main.html', context)
+
+
+@login_required
+def tasks_view(request, todo_id):
+    if request.method == 'POST':
+        task_id = request.POST.get('_id')
+        task = Task.objects.get(id=task_id)
+        if request.POST.get('value') == 'true':
+            task.task_is_finished = True
+            print("task.task_is_finised=True")
+        else:
+            task.task_is_finished = False
+            print("task.task_is_finised=False")
+        task.save()
+
+    todo = get_object_or_404(Todo, pk=todo_id)
+    tasks = Task.objects.filter(todo=todo)
+    context = {
+        'todo': todo,
+        'tasks': tasks
+    }
+    return render(request, 'main/todo_tasks.html', context)
