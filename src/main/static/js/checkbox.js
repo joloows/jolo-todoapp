14d3e92cec884b1a8b64285b4e131ed59fff923b
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-17 22:54:24 +0800

    Add Ajax for checkbox
    Created a view and template for Task objects. Ajax POST request parameters:
    '_id' - object.id
    'value' - custom value 'true' or 'false'

---
+++ src/main/static/js/checkbox.js
@@ -0,0 +1,18 @@
+$(".chkbox").click(function() {
+    var id = $(this).attr('id');
+    var value = $(this).attr('value');
+    if (value == "true") {
+        value = "false";
+    } else {
+        value = "true";
+    }
+    $.ajax({
+        url: '',
+        type: 'post',
+        data: {
+            'csrfmiddlewaretoken': csrftoken,
+            _id: id,
+            value: value
+        }
+    })
+})
