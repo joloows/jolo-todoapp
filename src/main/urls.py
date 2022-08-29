03b983b2b4639ba63865b1187b4bd6a7839c5bbb
John Carlo Del Rosario <djoloows@gmail.com>
2022-08-29 22:10:19 +0800

    Create views for Todo CRUD

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,12 +3,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
-# from .views import CreateTodoView
 
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('main/<int:todo_id>/', views.tasks_view, name='todo'),
-    # path('main/create/', CreateTodoView.as_view(), name='todo-create')
     path('main/create/', views.create_todo, name='todo-create'),
     path('main/<int:todo_id>/edit/', views.update_todo, name='todo-edit'),
     path('main/<int:todo_id>/delete/', views.delete_todo, name='todo-delete'),
