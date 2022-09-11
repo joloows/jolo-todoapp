fd90cddd8542b4f5f05df37633e469eed98e246c
John Carlo Del Rosario <djoloows@gmail.com>
2022-09-11 16:17:04 +0800

    Add homepage

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('main/', views.main, name='main'),
     path('main/<int:todo_id>/', views.tasks_view, name='todo'),
     path('main/create/', views.create_todo, name='todo-create'),
-    path('main/<int:todo_id>/edit/', views.update_todo, name='todo-update'),
+    path('main/<int:todo_id>/update/', views.update_todo, name='todo-update'),
     path('main/<int:todo_id>/delete/', views.delete_todo, name='todo-delete'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
